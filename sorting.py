11c410e12fc3fab1b9d8f5fbfad0e80849b18171
vijayjayakumar02 <76395344+vijayjayakumar02@users.noreply.github.com>
2021-01-07 20:14:41 +0530

    sorting the elements and also printing the swap count

---
+++ sorting.py
@@ -0,0 +1,9 @@
+a = list(map(int, input().strip().split()))
+swapCount = 0
+for i in range(len(a)):
+    for j in range(len(a) - 1):
+        if a[j] > a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+            swapCount += 1
+print(a)
+print(swapCount)
