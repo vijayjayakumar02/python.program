7e6478616e9fd824dc52d1b822c47d466db26c21
vijayjayakumar02 <76395344+vijayjayakumar02@users.noreply.github.com>
2021-01-08 14:21:28 +0530

    stack implementation

---
+++ stack.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+
+class stack:
+    def __init__(self):
+        self.container =deque()
+
+    def push(self, data):
+        self.container.append(data)
+
+    def pop(self):
+        self.container.pop()
+
+    def size(self):
+        return len(self.container)
+
+    def is_empty(self):
+        if len(self.container) == 0:
+            return 'Empty'
+        else:
+            return 'Not Empty'
+
+    def print(self):
+        print(self.container)
+
+
+ss = stack()
+ss.push(56)
+ss.push(85)
+ss.pop()
+ss.pop()
+ss.print()
+print(ss.is_empty())
